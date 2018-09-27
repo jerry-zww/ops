c75e35e0e00f033d43f68ce740990f61891dc809
Jerry Zhang <jerry-zww@users.noreply.github.com>
2018-09-27 16:52:31 +0800

    Create prepareEnv.sh

---
+++ prepareEnv.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Download Docker
+curl -sSL https://get.docker.com/ | sh
+
+yum install -y docker-ce
+
+#Install Docker Service & Start
+systemctl enable docker
+systemctl start docker
+
+#Add composer user and setup permissions
+adduser composer
+
+usermod -aG docker composer
+
+setfacl -m user:composer:rw /var/run/docker.sock
+
+echo 'composer ALL=(ALL) NOPASSWD:ALL' > user-composer
+
+#Install nvm and nodejs
+su composer
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+
+source ~/.bashrc
+
+nvm install v8.11.4
+
+nvm alias default v8.11.4
